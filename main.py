a3a06e6cf94846810970addae82ac16bdb706253
Alessandro Partesotti <alessandro.partesotti@fracarro.com>
2024-03-06 12:47:37 +0100

    auto name

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ import price_manager as pm
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python main.py <stock> <start_date> <end_date> <filename>")
+    if len(sys.argv) != 4:
+        print("Usage: python main.py <stock> <start_date> <end_date>")
+        
         sys.exit(1)
 
     stock = sys.argv[1]
     date_start = sys.argv[2]
     date_end = sys.argv[3]
-    filename = sys.argv[4]
+
 
     price = pm.get_edo_stock_price(stock, date_start, date_end)
-
+    filename = sys.argv[1] + "_from_{date_start}_to_{date_end}.csv".format(date_start=date_start, date_end=date_end)
     with open(filename, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(["Date", "Price"])
